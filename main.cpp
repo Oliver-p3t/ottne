a4dd1f2e509a8d8cced4620c3a151555f3e197ff
Oliver-p3t <petkesoliver7@gmail.com>
2025-12-18 08:34:04 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int joc(int n){
+int k=0;
+for(int i=2;i<=n;i++){
+    if(n%i==0){
+        k++;
+    }
+}
+return k;
+}
+int main()
+{
+   int n;
+   cin>>n;
+   cout<<joc(n);
+    return 0;
+}
